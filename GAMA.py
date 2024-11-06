42aa0fa4700761a499eb2695cc9b06515440ebde
zyx-uan <67188159+zyx-uan@users.noreply.github.com>
2024-11-06 10:46:13 +0800

    Update GAMA.py
    Add disclaimer

--- GAMA.py
+++ GAMA.py
@@ -17,6 +17,17 @@ st.set_page_config(
 if not check_password():  
     st.stop()
 
+# Add disclaimer section
+with st.expander("IMPORTANT NOTICE"):
+    st.write("""
+        This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+        Always consult with qualified professionals for accurate and personalized advice.
+    """)
+
+st.title("GenAI Access Management Assistant (GAMA)")
 
 st.title("GenAI Access Management Assistant (GAMA)")
 
